8ec4d3f62b20f9216cac12d73c679b5f9e0feaf0
IvanTerentevv <94695795+IvanTerentevv@users.noreply.github.com>
2022-04-21 19:39:47 +0300

    photo

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from waitress import serve
 import os
 import json
 from random import randint
+import sys
+import requests
 
 
 app = Flask(__name__)
@@ -191,6 +193,29 @@ def add_all_objects():
                              photo=photo)
                 db_sess.add(obj)
                 db_sess.commit()
+
+
+def get_photo(sel):
+    geocoder_request = \
+        f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={sel}&format=json"
+    response = requests.get(geocoder_request)
+    if response:
+        toponym_coodrinates = \
+            response.json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"].split()
+
+    map_request = \
+        f"http://static-maps.yandex.ru/1.x/?ll={toponym_coodrinates[0]},{toponym_coodrinates[1]}&spn=0.001,0.001&l=map"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "photo/map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
 
 
 def main():
