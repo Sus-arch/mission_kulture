d7b9a7b4dbff1acb3acf91f75d29c238bde11f19
Андрей <andrej5122785@yandex.ru>
2022-04-21 13:37:52 +0300

    авторизация

--- main.py
+++ main.py
@@ -68,7 +68,17 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 def main():
