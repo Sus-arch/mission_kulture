3437b95b07cb8c827b6568de3d5279b8d6c267c1
Андрей <andrej5122785@yandex.ru>
2022-04-21 10:14:01 +0300

    добавлена модель пользователей

---
+++ data/users.py
@@ -0,0 +1,27 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+from flask_login import LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    nick = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    is_admin = sqlalchemy.Column(sqlalchemy.BOOLEAN)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
